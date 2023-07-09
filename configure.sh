3186ef363255b00785f7144d4d049522252571be
Holden Hoover <holdenhoover@bkaw.ca>
2023-07-09 15:15:56 -0300

    Try other port

--- configure.sh
+++ configure.sh
@@ -131,7 +131,7 @@ UAT_INPUT="127.0.0.1:30978"
 RESULTS="--results beast,connect,127.0.0.1:30104"
 RESULTS2="--results basestation,listen,31003"
 RESULTS3="--results beast,listen,30157"
-RESULTS4="--results beast,connect,127.0.0.1:30154"
+RESULTS4="--results beast,connect,127.0.0.1:30155"
 # add --privacy between the quotes below to disable having the feed name shown on the mlat map
 # (position is never shown accurately no matter the settings)
 PRIVACY=""
@@ -139,7 +139,7 @@ INPUT_TYPE="$INPUT_TYPE"
 
 MLATSERVER="feed.radarplane.com:31090"
 TARGET="--net-connector feed.radarplane.com,30001,beast_reduce_out"
-NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30154 --net-bo-port 0 --net-ri-port 0 --write-json-every 1"
+NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30155 --net-bo-port 0 --net-ri-port 0 --write-json-every 1"
 JSON_OPTIONS="--max-range 450 --json-location-accuracy 2 --range-outline-hours 24"
 EOF
 
